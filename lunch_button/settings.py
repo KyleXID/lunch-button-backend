8c17a03618c8667e5ea4f3b916ef8e97dbf891fd
KyleXID <assaboy112@gmail.com>
2019-05-29 01:55:31 +0900

    django-cors 설정
    pip이용하여 django-cors-headers 설치함.
    settings파일에 cors 관련 셋팅을 추가함.

--- lunch_button/settings.py
+++ lunch_button/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'users',
     'community',
     'topics',
@@ -56,6 +57,8 @@ MIDDLEWARE = [
 #    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'lunch_button.urls'
@@ -129,3 +132,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Django-cors-headers
+#CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+    'localhost:3001',
+    'localhost:3002',
+    '127.0.0.1:3000',
+    '127.0.0.1:3001',
+    '127.0.0.1:3002',
+)
